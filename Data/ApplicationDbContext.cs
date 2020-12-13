b3ffab8d248aa8658792cfd450f08cb0d706c4cf
jantubeeckx <jan.tubeeckx@student.hogent.be>
2020-12-13 17:12:58 +0100

    Found solution  renaming appsettings.json

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerHallEF.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionstring = @"Server=localhost,1433;User Id=sa;Password=Mezta840;Database=Beerhall;Integrated Security=true";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BeerHallEF.Data;
+
+namespace BeerHallEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(ApplicationDbContext context = new ApplicationDbContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                Console.WriteLine("Database created");
+            }
+        }
+    }
+}
